3ea9bf00273d873ed55be05b0834dd7d402b040c
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-03-04 19:26:52 +0000

    glowing

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 
 export function Home() {
-	const [state, setState] = useState({
-		red: "",
-		yellow: "",
-		green: ""
-	});
+	const [color, setColor] = useState("red");
 	const stylesBorder = {
-		border: "1px solid black",
-		height: "30px",
-		width: "30px"
+		border: "1px solid black"
 	};
+	const allColors = ["danger", "warning", "success"];
 	return (
 		<div
 			className="card"
@@ -19,39 +14,14 @@ export function Home() {
 				width: "32px",
 				margin: "auto"
 			}}>
-			<div
-				className="rounded-circle bg-danger border-dark"
-				style={
-					!state.red
-						? {
-								height: "30px",
-								width: "30px"
-						  }
-						: stylesBorder
-				}
-				onClick={() =>
-					setState({ red: "clicked", yellow: "", green: "" })
-				}></div>
-			<div
-				className="rounded-circle bg-warning border-dark"
-				style={
-					state.yellow
-						? stylesBorder
-						: { height: "30px", width: "30px" }
-				}
-				onClick={() =>
-					setState({ red: "", yellow: "clicked", green: "" })
-				}></div>
-			<div
-				className="rounded-circle bg-success border-dark"
-				style={
-					state.green
-						? stylesBorder
-						: { height: "30px", width: "30px" }
-				}
-				onClick={() =>
-					setState({ red: "", yellow: "", green: "clicked" })
-				}></div>
+			{allColors.map((c, i) => (
+				<div
+					key={i}
+					className={`rounded-circle bg-${c} border-dark ${
+						c == color ? "glow" : ""
+					}`}
+					onClick={() => setColor(c)}></div>
+			))}
 		</div>
 	);
 }
